a24a8823face8b40c2bfefd85f5277bc313a6e13
Alex Herbert <aherbert@apache.org>
2024-02-13 08:00:52 +0000

    DoubleDataTransformers

--- commons-statistics-examples/examples-jmh/src/main/java/org/apache/commons/statistics/examples/jmh/descriptive/DoubleDataTransformers.java
+++ commons-statistics-examples/examples-jmh/src/main/java/org/apache/commons/statistics/examples/jmh/descriptive/DoubleDataTransformers.java
@@ -16,6 +16,7 @@
  */
 package org.apache.commons.statistics.examples.jmh.descriptive;
 
+import java.util.Arrays;
 import java.util.function.Supplier;
 
 /**
@@ -29,161 +30,201 @@ final class DoubleDataTransformers {
     private DoubleDataTransformers() {}
 
     /**
-     * Creates a factory to supply {@link DoubleDataTransformer} based on the
+     * Creates a factory to supply a {@link DoubleDataTransformer} based on the
      * {@code nanPolicy} and data {@code copy} policy.
+     *
+     * <p>The factory will supply instances that may be reused on the same thread.
+     * Multi-threaded usage should create an instance per thread.
      *
      * @param nanPolicy NaN policy.
      * @param copy Set to {@code true} to use a copy of the data.
      * @return the factory
      */
     static Supplier<DoubleDataTransformer> createFactory(NaNPolicy nanPolicy, boolean copy) {
-        // TODO:
-        // choose a constructor for a suitable implementation
-        return null;
+        if (nanPolicy == NaNPolicy.ERROR) {
+            return () -> new NaNErrorTransformer(copy);
+        }
+        // Support including NaN / exclusing NaN from the data size
+        final boolean includeNaN = nanPolicy == NaNPolicy.INCLUDE;
+        return () -> new SortTransformer(includeNaN, copy);
     }
 
-//  /**
-//  * Sort {@code NaN} values and count signed zeros. Any signed zero is replaced with {@code 0.0}.
-//  *
-//  * <p>Data can be repaired after any reordering by a parition method using
-//  * {@link #replaceSignedZeros(double[], int, int)}; or a sort by
-//  * {@link #replaceContinuousSignedZeros(double[], int, int)}.
-//  *
-//  * <p>Data transformation returns:
-//  * <ul>
-//  * <li>Low order 32-bits: length of the processed array without {@code NaN}
-//  * <li>High order 32-bits: count of signed zeros
-//  * </ul>
-//  */
-// SORT_NAN_COUNT_ZEROS {
-//     @Override
-//     long transform(double[] a) {
-//         // Sort NaN / count signed zeros
-//         int cn = 0;
-//         int end = a.length;
-//         for (int i = end; i > 0;) {
-//             final double v = a[--i];
-//             // Count negative zeros using a sign bit check.
-//             // This requires a performance test. If the conversion to raw bits
-//             // is natively supported this is faster than using the == check.
-//             //if (v == 0.0 && Double.doubleToRawLongBits(v) < 0) {
-//             if (Double.doubleToRawLongBits(v) == Long.MIN_VALUE) {
-//                 cn++;
-//                 // Change to positive zero.
-//                 // Data must be repaired after sort.
-//                 a[i] = 0.0;
-//             } else if (v != v) {
-//                 // Move NaN to end
-//                 a[i] = a[--end];
-//                 a[end] = v;
-//             }
-//         }
-//         // pack [count of signed zeros, length of data]
-//         return (((long) cn) << Integer.SIZE) | end;
-//     }
-// },
-// /**
-//  * Raise an {@link IllegalArgumentException} for {@code NaN} values and count signed zeros.
-//  */
-// ERROR_NAN_COUNT_ZEROS {
-//     @Override
-//     double[] prepare(double[] data, boolean copy) {
-//         // TODO Auto-generated method stub
-//         return super.prepare(data, copy);
-//     }
-//
-//     @Override
-//     long transform(double[] a) {
-//         // Here we delay copy to not change the data if a NaN is found.
-//         // But we commit to a double scan for signed zeros.
-//         double[] a = data;
-//         // Sort NaN / count signed zeros
-//         int cn = 0;
-//         final int end = a.length;
-//         for (int i = end; i > 0;) {
-//             final double v = a[--i];
-//             // Count negative zeros using a sign bit check.
-//             // This requires a performance test. If the conversion to raw bits
-//             // is natively supported this is faster than using the == check.
-//             //if (v == 0.0 && Double.doubleToRawLongBits(v) < 0) {
-//             if (Double.doubleToRawLongBits(v) == Long.MIN_VALUE) {
-//                 cn++;
-//             } else if (v != v) {
-//                 throw new IllegalArgumentException("NaN at: " + i);
-//             }
-//         }
-//         if (copy) {
-//
-//         }
-//         // pack [count of signed zeros, length of data]
-//         final long result = (((long) cn) << Integer.SIZE) | a.length;
-//         // Re-write zeros if required
-//         if (cn != 0) {
-//             for (int i = end; i > 0;) {
-//                 final double v = a[--i];
-//                 // Count negative zeros using a sign bit check.
-//                 // This requires a performance test. If the conversion to raw bits
-//                 // is natively supported this is faster than using the == check.
-//                 //if (v == 0.0 && Double.doubleToRawLongBits(v) < 0) {
-//                 if (Double.doubleToRawLongBits(v) == Long.MIN_VALUE) {
-//                     cn++;
-//                 }
-//             }
-//         }
-//         return result;
-//     }
-// };
-
     /**
-     * Replace the first {@code count} occurrences of zero with {@code -0.0}
-     * starting after the provided {@code from} index.
-     *
-     * <p>Zeros after {@code from} may be discontinuous, for example in data
-     * that is partitioned.
-     *
-     * <p>Warning: This method assumes that there are at least {@code count} zeros in
-     * the range {@code [from + 1, a.length)}, otherwise an index out of bounds exception will
-     * occur as the scan passes the end of the data.
-     *
-     * @param a Data.
-     * @param from Index before the position to start the repair; use -1 for the start of the array.
-     * @param count Count of signed zeros (assumed to be strictly positive).
+     * A transformer that moves {@code NaN} to the upper end of the array.
+     * Signed zeros are counted.
      */
-    static void replaceSignedZeros(double[] a, int from, int count) {
-        // Assume the zeros are all present so no bounds checks
-        // are used when incrementing j. But we have to check for zeros
-        // before overwrite.
-        for (int j = from, cn = count;;) {
-            if (a[++j] == 0) {
-                a[j] = -0.0;
-                if (--cn == 0) {
-                    break;
+    private abstract static class ReplaceSignedZerosTransformer implements DoubleDataTransformer {
+        /** Count of negative zeros. */
+        protected int negativeZeroCount;
+
+        @Override
+        public void postProcess(double[] data, int[] k, int n) {
+            // Restore signed zeros
+            if (negativeZeroCount != 0) {
+                // Use the partitioned indices to fast-forward as much as possible.
+                // Assumes partitioning has not changed indices (but
+                // reordering is OK).
+                int j = -1;
+                for (int i = 0; i < n; i++) {
+                    if (k[i] < 0) {
+                        j = Math.max(j, k[i]);
+                    }
+                }
+                for (int cn = negativeZeroCount;;) {
+                    if (data[++j] == 0) {
+                        data[j] = -0.0;
+                        if (--cn == 0) {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        @Override
+        public void postProcess(double[] data) {
+            if (negativeZeroCount != 0) {
+                // Find a zero
+                int j = Arrays.binarySearch(data, 0.0);
+                // Scan back to before zero
+                while (--j >= 0) {
+                    if (data[j] != 0) {
+                        break;
+                    }
+                }
+                // Fix. Assume the zeros are all present so just overwrite
+                // the required count of signed zeros.
+                for (int cn = negativeZeroCount; --cn >= 0;) {
+                    data[++j] = -0.0;
                 }
             }
         }
     }
 
     /**
-     * Replace the first {@code count} occurrences of zero with {@code -0.0}
-     * starting after the provided {@code from} index.
-     *
-     * <p>It is assumed that {@code a[from + 1] == 0} and zeros after are continuous,
-     * for example in data that is sorted, or partitioned around zero using a method
-     * that collects equal value together.
-     *
-     * <p>Warning: This method assumes that there are at least {@code count} zeros in
-     * the range {@code [from + 1, a.length)}, otherwise an index out of bounds exception will
-     * occur as the scan passes the end of the data.
-     *
-     * @param a Data.
-     * @param from Index to start the repair.
-     * @param count Count of signed zeros (assumed to be strictly positive).
+     * A transformer that moves {@code NaN} to the upper end of the array.
+     * Signed zeros are counted.
      */
-    static void replaceContinuousSignedZeros(double[] a, int from, int count) {
-        // Assume the zeros are continuous so just overwrite
-        // the required count of signed zeros.
-        for (int j = from, cn = count; --cn >= 0;) {
-            a[++j] = -0.0;
+    private static final class SortTransformer extends ReplaceSignedZerosTransformer {
+        /** Set to {@code true} to include NaN in the size of the data. */
+        private final boolean includeNaN;
+        /** Set to {@code true} to use a copy of the data. */
+        private final boolean copy;
+        /** Size of the data. */
+        private int size;
+        /** Length of data to partition. */
+        private int len;
+
+        /**
+         * @param includeNaN Set to {@code true} to include NaN in the size of the data.
+         * @param copy Set to {@code true} to use a copy of the data.
+         */
+        private SortTransformer(boolean includeNaN, boolean copy) {
+            this.includeNaN = includeNaN;
+            this.copy = copy;
+        }
+
+        @Override
+        public double[] preProcess(double[] data) {
+            final double[] a = copy ? data.clone() : data;
+            // Sort NaN / count signed zeros
+            int cn = 0;
+            int end = a.length;
+            for (int i = end; i > 0;) {
+                final double v = a[--i];
+                // Count negative zeros using a sign bit check.
+                // This requires a performance test. If the conversion to raw bits
+                // is natively supported this is faster than using the == check.
+                // if (v == 0.0 && Double.doubleToRawLongBits(v) < 0) {
+                if (Double.doubleToRawLongBits(v) == Long.MIN_VALUE) {
+                    cn++;
+                    // Change to positive zero.
+                    // Data must be repaired after sort.
+                    a[i] = 0.0;
+                } else if (v != v) {
+                    // Move NaN to end
+                    a[i] = a[--end];
+                    a[end] = v;
+                }
+            }
+            negativeZeroCount = cn;
+            len = end;
+            size = includeNaN ? a.length : len;
+            return a;
+        }
+
+        @Override
+        public int size() {
+            return size;
+        }
+
+        @Override
+        public int length() {
+            return len;
+        }
+    }
+
+    /**
+     * A transformer that errors on {@code NaN}.
+     * Signed zeros are counted and restored.
+     */
+    private static final class NaNErrorTransformer extends ReplaceSignedZerosTransformer {
+        /** Set to {@code true} to use a copy of the data. */
+        private final boolean copy;
+        /** Size of the data. */
+        private int size;
+
+        /**
+         * @param copy Set to {@code true} to use a copy of the data.
+         */
+        private NaNErrorTransformer(boolean copy) {
+            this.copy = copy;
+        }
+
+        @Override
+        public double[] preProcess(double[] data) {
+            // Here we delay copy to not change the data if a NaN is found.
+            // But we commit to a double scan for signed zeros.
+            double[] a = data;
+            // Error on NaN / count signed zeros
+            int cn = 0;
+            for (int i = a.length; i > 0;) {
+                final double v = a[--i];
+                // This requires a performance test
+                if (Double.doubleToRawLongBits(v) == Long.MIN_VALUE) {
+                    cn++;
+                } else if (v != v) {
+                    throw new IllegalArgumentException("NaN at: " + i);
+                }
+            }
+            negativeZeroCount = cn;
+            size = a.length;
+            // No NaNs so copy the data if required
+            if (copy) {
+                a = a.clone();
+            }
+            // Re-write zeros if required
+            if (cn != 0) {
+                for (int i = a.length; i > 0;) {
+                    if (Double.doubleToRawLongBits(a[--i]) == Long.MIN_VALUE) {
+                        a[i] = 0.0;
+                        if (--cn == 0) {
+                            break;
+                        }
+                    }
+                }
+            }
+            return a;
+        }
+
+        @Override
+        public int size() {
+            return size;
+        }
+
+        @Override
+        public int length() {
+            return size;
         }
     }
 }
