7297b646769a5cd9c11ee17e097a7b0ac466fce3
Alex Herbert <aherbert@apache.org>
2024-02-25 08:01:19 +0000

    Use sampled indices to test PivotingStrategy

--- commons-statistics-examples/examples-jmh/src/main/java/org/apache/commons/statistics/examples/jmh/descriptive/PivotingStrategy.java
+++ commons-statistics-examples/examples-jmh/src/main/java/org/apache/commons/statistics/examples/jmh/descriptive/PivotingStrategy.java
@@ -36,6 +36,16 @@ enum PivotingStrategy {
         int pivotIndex(int[] data, int left, int right) {
             return (left + right) >>> 1;
         }
+
+        @Override
+        int[] getSampledIndices(int left, int right) {
+            return new int[] {(left + right) >>> 1};
+        }
+
+        @Override
+        int samplingEffect() {
+            return UNCHANGED;
+        }
     },
     /**
      * Pivot around the median of 3 values within the range: the first; the centre; and the last.
@@ -49,6 +59,16 @@ enum PivotingStrategy {
         @Override
         int pivotIndex(int[] data, int left, int right) {
             return med3(data, left, (left + right) >>> 1, right);
+        }
+
+        @Override
+        int[] getSampledIndices(int left, int right) {
+            return new int[] {left, (left + right) >>> 1, right};
+        }
+
+        @Override
+        int samplingEffect() {
+            return UNCHANGED;
         }
     },
     /**
@@ -78,6 +98,22 @@ enum PivotingStrategy {
             final int z = med3(data, right - (s << 1), right - s, right);
             return med3(a, b, data[z], x, y, z);
         }
+
+        @Override
+        int[] getSampledIndices(int left, int right) {
+            final int s = (right - left) >>> 3;
+            final int m = (left + right) >>> 1;
+            return new int[] {
+                left, left + s, left + (s << 1),
+                m - s, m, m + s,
+                right - (s << 1), right - s, right
+            };
+        }
+
+        @Override
+        int samplingEffect() {
+            return UNCHANGED;
+        }
     },
     /**
      * Pivot around the median of 3 or 9 values within the range.
@@ -102,21 +138,36 @@ enum PivotingStrategy {
             }
             return MEDIAN_OF_3.pivotIndex(data, left, right);
         }
+
+        @Override
+        int[] getSampledIndices(int left, int right) {
+            if (right - left >= MED_9) {
+                return MEDIAN_OF_9.getSampledIndices(left, right);
+            }
+            return MEDIAN_OF_3.getSampledIndices(left, right);
+        }
+
+        @Override
+        int samplingEffect() {
+            return UNCHANGED;
+        }
     },
     /**
      * Pivot around the median of 5 values within the range.
      * Requires that {@code right - left >= 4}.
      *
      * <p>Warning: This has the side effect that the 5 values are also sorted.
+     *
+     * <p>Uses the same spacing as {@link DualPivotingStrategy#SORT_5}.
      */
     MEDIAN_OF_5 {
         @Override
         int pivotIndex(double[] data, int left, int right) {
-            // Here we sort 5 points and choose 2 and 4 as the pivots: 1/6, 1/3, 1/2, 2/3, 5/6
-            // 1/6 ~ 1/8 + 1/32. Ensure the value is above zero to choose different points!
+            // 1/6 = 5/30 ~ 1/8 + 1/32 + 1/64 : 0.1666 ~ 0.1719
+            // Ensure the value is above zero to choose different points!
             // This is safe if len >= 4.
             int len = right - left;
-            int sixth = 1 + (len >>> 3) + (len >>> 5);
+            final int sixth = 1 + (len >>> 3) + (len >>> 5) + (len >>> 6);
             int p3 = left + (len >>> 1);
             int p2 = p3 - sixth;
             int p1 = p2 - sixth;
@@ -130,8 +181,76 @@ enum PivotingStrategy {
         int pivotIndex(int[] data, int left, int right) {
             throw new IllegalStateException("Unsupported");
         }
+
+        @Override
+        int[] getSampledIndices(int left, int right) {
+            final int len = right - left;
+            final int sixth = 1 + (len >>> 3) + (len >>> 5) + (len >>> 6);
+            final int p3 = left + (len >>> 1);
+            final int p2 = p3 - sixth;
+            final int p1 = p2 - sixth;
+            final int p4 = p3 + sixth;
+            final int p5 = p4 + sixth;
+            return new int[] {p1, p2, p3, p4, p5};
+        }
+
+        @Override
+        int samplingEffect() {
+            return SORT;
+        }
+    },
+    /**
+     * Pivot around the median of 5 values within the range.
+     * Requires that {@code right - left >= 4}.
+     *
+     * <p>Warning: This has the side effect that the 5 values are also sorted.
+     *
+     * <p>Uses the same spacing as {@link DualPivotingStrategy#SORT_5B}.
+     */
+    MEDIAN_OF_5B {
+        @Override
+        int pivotIndex(double[] data, int left, int right) {
+            // 1/7 = 5/35 ~ 1/8 + 1/64 : 0.1429 ~ 0.1406
+            // Ensure the value is above zero to choose different points!
+            // This is safe if len >= 4.
+            final int len = right - left;
+            final int seventh = 1 + (len >>> 3) + (len >>> 6);
+            final int p3 = left + (len >>> 1);
+            final int p2 = p3 - seventh;
+            final int p1 = p2 - seventh;
+            final int p4 = p3 + seventh;
+            final int p5 = p4 + seventh;
+            Sorting.sort5(data, p1, p2, p3, p4, p5);
+            return p3;
+        }
+
+        @Override
+        int pivotIndex(int[] data, int left, int right) {
+            throw new IllegalStateException("Unsupported");
+        }
+
+        @Override
+        int[] getSampledIndices(int left, int right) {
+            final int len = right - left;
+            final int seventh = 1 + (len >>> 3) + (len >>> 6);
+            final int p3 = left + (len >>> 1);
+            final int p2 = p3 - seventh;
+            final int p1 = p2 - seventh;
+            final int p4 = p3 + seventh;
+            final int p5 = p4 + seventh;
+            return new int[] {p1, p2, p3, p4, p5};
+        }
+
+        @Override
+        int samplingEffect() {
+            return SORT;
+        }
     };
 
+    /** Sampled points are unchanged. */
+    static final int UNCHANGED = 0;
+    /** Sampled points are sorted. */
+    static final int SORT = 0x2;
     /** Size to pivot around the median of 9. */
     private static final int MED_9 = 40;
 
@@ -232,4 +351,27 @@ enum PivotingStrategy {
      * element of the array slice
      */
     abstract int pivotIndex(int[] data, int left, int right);
+
+    // The following methods allow the strategy and side effects to be tested
+
+    /**
+     * Get the indices of points that will be sampled.
+     *
+     * @param left Lower bound (inclusive).
+     * @param right Upper bound (inclusive).
+     * @return the indices
+     */
+    abstract int[] getSampledIndices(int left, int right);
+
+    /**
+     * Get the effect on the sampled points.
+     * <ul>
+     * <li>0 - Unchanged
+     * <li>1 - Partially sorted
+     * <li>2 - Sorted
+     * </ul>
+     *
+     * @return the effect
+     */
+    abstract int samplingEffect();
 }
