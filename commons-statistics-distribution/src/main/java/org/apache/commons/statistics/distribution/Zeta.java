99c31148ac03bc0224b0f2b1da7c07ed74a6885c
Alex Herbert <aherbert@apache.org>
2026-09-03 18:22:10 +0100

    Initial zeta class

---
+++ commons-statistics-distribution/src/main/java/org/apache/commons/statistics/distribution/Zeta.java
@@ -0,0 +1,172 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.commons.statistics.distribution;
+
+/**
+ * Utility class used to compute the
+ * <a href="https://en.wikipedia.org/wiki/Hurwitz_zeta_function">Hurwitz zeta</a> function.
+ *
+ * <pre>
+ *                 oo    1
+ * zeta(s, a) = sum    ------
+ *                 k=0      s
+ *                     (k+a)
+ * </pre>
+ *
+ * <p>The function is formally defined for complex variable {@code s} with {@code Re(s) > 1}
+ * and real {@code a != 0, -1, -2, ...}. This series is absolutely convergent for the given
+ * values of {@code a} and {@code a}.
+ *
+ * <p>This implementation uses real-valued {@code s} and {@code a} as a positive integer.
+ * Specialisation to a smaller domain than any finite {@code a} allows optimisation for
+ * a {@code double} precision result.
+ *
+ * <p>The implementation is performed by spitting the integral into two parts and using
+ * the Euler-Maclaurin formula to approximate the second integral {@code I + T + R}
+ * with a continuous integral {@code I}, a tail {@code T}, and a residual error term
+ * {@code R}.
+ *
+ * <pre>
+ *                 N-1           oo
+ * zeta(s, a) = sum    f(k) + sum    f(k) = S + I + T + R
+ *                 k=0           k=N
+ *
+ *          1
+ * f(k) = ------
+ *             s
+ *        (a+k)
+ *
+ *                            1-s
+ *      ,-oo   1         (a+N)
+ * I =  |    ------ dt = --------
+ *     -' N       s        s-1
+ *           (a+t)
+ *            /             B     (s)      \
+ *       1    | 1      M     2k      2k-1  |
+ * T = ------ | - + sum    ----- --------- |
+ *          s | 2      k=1 (2k)!      2k-1 |
+ *     (a+N)  \                  (a+N)     /
+ *
+ * B   = Bernoulli number
+ *  2k
+ *           ___n-1
+ * (s)     = | |    (x+i)    (rising factorial Pochhammer function)
+ *    n      | |i=0
+ * </pre>
+ *
+ * <p>These formulas for the real-valued {@code s} are provided in Johansson (2015) as
+ * equations 5-9. The implementation omits the residual term {@code R}.
+ *
+ * <p>References
+ * <ol>
+ * <li>Johansson (2015)
+ * Rigorous high-precision computation of the Hurwitz zeta function and its derivatives
+ * <a href="https://link.springer.com/article/10.1007/s11075-014-9893-1">Numerical Algorithms (69) 253–270</a></li>
+ * <li><a href="https://en.wikipedia.org/wiki/Hurwitz_zeta_function">Hurwitz zeta function (Wikipedia)</a></li>
+ * <li><a href="https://en.wikipedia.org/wiki/Euler%E2%80%93Maclaurin_formula">Euler–Maclaurin formula (Wikipedia)</a></li>
+ * <li><a href="https://en.wikipedia.org/wiki/Bernoulli_number">Bernoulli number (Wikipedia)</a></li>
+ * <li><a href="https://en.wikipedia.org/wiki/Falling_and_rising_factorials">Rising and falling factorirals (Wikipedia)</a></li>
+ * </ol>
+ *
+ * @since 1.4
+ */
+final class Zeta {
+    /**
+     * Precomputed factors for {@code k}-th element of the tail function {@code T}.
+     * Uses {@code 2k!} divided by Bernoulli number {@code B_2k}.
+     * The table size is suitable for N ~ M ~ P for P-bits of precision (53 entries)
+     * as stated in Johansson (2015) section 3.1.
+     */
+    static final double[] F = {
+        12.0, // 2! / (1 / 6)
+        -720.0, // 4! / (-1 / 30)
+        30240.0, // 6! / (1 / 42)
+        -1209600.0, // 8! / (-1 / 30)
+        4.790016E7, // 10! / (5 / 66)
+        -1.8924375803183792E9, // 12! / (-691 / 2730)
+        7.47242496E10, // 14! / (7 / 6)
+        -2.950130727918164E12, // 16! / (-3617 / 510)
+        1.1646782814350067E14, // 18! / (43867 / 798)
+        -4.597978722407473E15, // 20! / (-174611 / 330)
+        1.81521054019435456E17, // 22! / (854513 / 138)
+        -7.1661652561756672E18, // 24! / (-236364091 / 2730)
+        2.82908877253043E20, // 26! / (8553103 / 6)
+        -1.1168794925000445E22, // 28! / (-23749461029 / 870)
+        4.4092635141854666E23, // 30! / (8615841276005 / 14322)
+        -1.7407074646225822E25, // 32! / (-7709321041217 / 510)
+        6.872037622739274E26, // 34! / (2577687858367 / 6)
+        -2.7129717107520044E28, // 36! / (-26315271553053477373 / 1919190)
+        1.0710383014704457E30, // 38! / (2929993913841559 / 6)
+        -4.228289733582729E31, // 40! / (-261082718496449122051 / 13530)
+        1.669261878547101E33, // 42! / (1520097643918070802691 / 1806)
+        -6.589981753232787E34, // 44! / (-27833269579301024235023 / 690)
+        2.6016205165923062E36, // 46! / (596451111593912163277961 / 282)
+        -1.0270786120209628E38, // 48! / (-5609403368997817686249127547 / 46410)
+        4.054743835786749E39, // 50! / (495057205241079648212477525 / 66)
+        -1.6007487042788347E41, // 52! / (-801165718135489957347924991853 / 1590)
+        6.319502582715391E42, // 54! / (29149963634884862421418123812691 / 798)
+        -2.4948396201225357E44, // 56! / (-2479392929313226753685415739663229 / 870)
+        9.849232037909393E45, // 58! / (84483613348880041862046775994036021 / 354)
+        -3.888320954748035E47, // 60! / (-1215233140483755572040304994079820246041491 / 56786730)
+        1.535047584313167E49, // 62! / (12300585434086858541953039857403386151 / 6)
+        -6.060124957607529E50, // 64! / (-106783830147866529886385444979142647942017 / 510)
+        2.3924414381101893E52, // 66! / (1472600022126335654051619428551932342241899101 / 64722)
+        -9.444980218768351E53, // 68! / (-78773130858718728141909149208474606244347001 / 30)
+        3.728728733414322E55, // 70! / (1505381347333367003803076567377857208511438160235 / 4686)
+        -1.4720431007109737E57, // 72! / (-5827954961669944110438277244641067365282488301844260429 / 140100870)
+        5.811393226148101E58, // 74! / (34152417289221168014330073731472635186688307783087 / 6)
+        -2.2942460864500874E60, // 76! / (-24655088825935372707687196040585199904365267828865801 / 30)
+        9.057320508803927E61, // 78! / (414846365575400828295179035549542073492199375372400483487 / 3318)
+        -3.575686814230726E63, // 80! / (-4603784299479457646935574969019046849794257872751288919656867 / 230010)
+        1.4116245727459505E65, // 82! / (1677014149185145836823154509786269900207736027570253414881613 / 498)
+        -5.5728704383437275E66, // 84! / (-2024576195935290360231131160111731009989917391198090877281083932477 / 3404310)
+        2.2000810641991213E68, // 86! / (660714619417678653573847847426261496277830686653388931761996983 / 6)
+        -8.685571901589203E69, // 88! / (-1311426488674017507995511424019311843345750275572028644296919890574047 / 61410)
+        3.428926346636115E71, // 90! / (1179057279021082799884123351249215083775254949669647116231545215727922535 / 272118)
+        -1.3536858624708422E73, // 92! / (-1295585948207537527989427828538576749659341483719435143023316326829946247 / 1410)
+        5.344137578373867E74, // 94! / (1220813806579744469607301679413201203958508415202696621436215105284649447 / 6)
+        -2.10978095054183E76, // 96! / (-211600449597266513097597728109824233673043954389060234150638733420050668349987259 / 4501770)
+        8.329081341920855E77, // 98! / (67908260672905495624051117546403605607342195728504487509073961249992947058239 / 6)
+        -3.288189514770133E79, // 100! / (-94598037819122125295227433069493721872702841533066936133385696204311395415197247711 / 33330)
+        1.2981251882636475E81, // 102! / (3204019410860907078243020782116241775491817197152717450679002501086861530836678158791 / 4326)
+        -5.124792828500739E82, // 104! / (-319533631363830011287103352796174274671189606078272738327103470162849568365549721224053 / 1590)
+        2.023187114193683E84, // 106! / (36373903172617414408151820151593427169231298640581690038930816378281879873386202346572901 / 642)
+    };
+
+    /** No instances. */
+    private Zeta() {}
+
+    /**
+     * Compute the value of the Hurwitz zeta function {@code zeta(s, a)}.
+     *
+     * <pre>
+     *                 oo    1
+     * zeta(s, a) = sum    ------
+     *                 k=0      s
+     *                     (k+a)
+     * </pre>
+     *
+     * <p><strong>Warning</strong>: No parameter validation is performed.
+     *
+     * @param s Argument {@code s > 1}
+     * @param a Argument {@code a >= 1}
+     * @return zeta(s, a)
+     */
+    static double value(double s, int a) {
+        return 0;
+    }
+}
